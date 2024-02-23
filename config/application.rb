3d4abd91208621c0872df5d47a90a719b11f9c53
Ryota <ryoenoki2013@gmail.com>
2024-02-23 21:36:45 +0100

    fix config: set default values

--- config/application.rb
+++ config/application.rb
@@ -23,17 +23,10 @@ module RailsWatchList
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    config.assets.enabled = true
-    config.assets.paths << Rails.root.join("app", "assets", "images")
-
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-
-    config.assets.configure do |env|
-      env.export_concurrent = false
-    end
 
     # Configuration for the application, engines, and railties goes here.
     #
